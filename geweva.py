83303f1afb7f838b373eee777d691d37a81f9e6c
Spencer Gunning <swcgunning@gmail.com>
2017-08-24 15:17:39 -0400

    [0.0.1-b1] Fixed while loop in header

--- geweva.py
+++ geweva.py
@@ -23,7 +23,7 @@ def twrite(tspace):
         sys.stdout.write(char)
         sys.stdout.flush()
 
-twrite(col.g + 'Geweva Plotter ' + col.d + '[0.0.1-a9]\n\n' + col.x + \
+twrite(col.g + 'Geweva Plotter ' + col.d + '[0.0.1-b1]\n\n' + col.x + \
 col.c + 'Please select an operation:\n' + col.x + \
 col.y + '    [0] Graph one month of data\n' + col.x + \
 col.y + '    [1] Inject new data\n' + col.x + \
@@ -31,233 +31,238 @@ col.y + '    [9] Exit Geweva Plotter\n\n' + col.x)
 
 while True:
     try:
-        twrite('>>> ')
+        twrite('\n>>> ')
         selectop = int(input())
 
-    except ValueError or (selectop is not [0, 1, 9]):
-        twrite(col.r + '\nIncorrect operation specified. Please retry.\n\n' + col.x)
+    except ValueError:
+        twrite(col.r + '\nIncorrect operation specified. Please retry.\n' + col.x)
         continue
-    
-    else:
-        break
 
-if selectop is 0:
-    twrite(col.y + '\nPlease enter a month to be graphed: ' + col.x)
+    if selectop is 0:
+        twrite(col.y + '\nPlease enter a month to be graphed: ' + col.x)
 
-    while True:
-        try:
-            mint = int(input())
-            def find_file(name):
-                for files in os.getcwd():
-                    glog = str(name) + 'vals.log'
-                    return str(name)
+        while True:
+            try:
+                mint = int(input())
+                def find_file(name):
+                    for files in os.getcwd():
+                        glog = str(name) + 'vals.log'
+                        return str(name)
 
-        except ValueError:
-            twrite(col.r + '\nInput was not an integer. Please retry: ' + col.x)
-            continue
+            except ValueError:
+                twrite(col.r + '\nInput was not an integer. Please retry: ' + col.x)
+                continue
 
-        try:
-            mstr = find_file(mint)
-            fto = mstr + 'vals.log'
-            os.path.isfile(fto)
-            f = open(fto, 'r')
+            try:
+                mstr = find_file(mint)
+                fto = mstr + 'vals.log'
+                os.path.isfile(fto)
+                f = open(fto, 'r')
 
-        except IOError:
-            twrite(col.r + '\nThe specified Geweva log was not found. Please retry: ' + col.x)
-            continue
+            except IOError:
+                twrite(col.r + '\nThe specified Geweva log was not found. Please retry: ' + col.x)
+                continue
+
+            else:
+                break
+
+        alldata = f.read().splitlines()
+        f.close()
+
+        xdata = np.array(alldata[0].split('  '), dtype=float)
+        ydata = np.array(alldata[1].split('  '), dtype=float)
+
+        if len(ydata) < 2:
+            twrite(col.r + '\nInsufficient information for graphing. Please add more data before attempting to produce a graph.\n\n' + col.x)
+            sys.exit()
+
+        xhi = np.amax(xdata)
+        xlo = np.amin(xdata)
+        yhi = np.amax(ydata) + 2.5
+        ylo = np.amin(ydata) - 2.5
+
+        month = cal.month_name[mint]
+        maxis = cal.monthrange(2017, mint)[1]
+
+        xply = np.polyfit(xdata, ydata, xhi)
+        yply = np.poly1d(xply)
+        xlin = np.polyfit(xdata, ydata, 1)
+        ylin = np.poly1d(xlin)
+
+        xnew = np.linspace(xlo, xhi, 300)
+        xalt = np.linspace(xlo, xhi, 50)
+        ynew = yply(xnew)
+        yalt = ylin(xalt)
+
+        blinx = np.linspace(xlo, maxis, 50)
+        bliny = eval('-5*blinx/19 + 203')
+        
+        if np.amax(bliny) + 2.5 > yhi:
+            yhighest = np.amax(bliny) + 2.5
+            
+        else:
+            yhighest = yhi
+        
+        if np.amin(bliny) - 2.5 < ylo:
+            ylowest = np.amin(bliny) - 2.5
 
         else:
-            break
+            ylowest = ylo
+        
+        twrite(col.c + '\nGraphing data')
+        time.sleep(0.5)
+        twrite('.')
+        time.sleep(0.7)
+        twrite('.')
+        time.sleep(0.7)
+        twrite('.\n\n' + col.x)
+        time.sleep(0.5)
 
-    alldata = f.read().splitlines()
-    f.close()
+        # dates = mcal.date2num('2017-' + mstr + '-1', '2017-' + mstr + '-' + str(maxis))
 
-    xdata = np.array(alldata[0].split('  '), dtype=float)
-    ydata = np.array(alldata[1].split('  '), dtype=float)
-
-    if len(ydata) < 2:
-        twrite(col.r + '\nInsufficient information for graphing. Please add more data before attempting to produce a graph.\n\n' + col.x)
+        fig, graph = plt.subplots()
+        graph.fill_between(blinx, bliny - 2.5, bliny + 2.5, color = 'b', alpha = 0.05)
+        lideal, = graph.plot(blinx, bliny, 'b')
+        lamove, = graph.plot(xnew, ynew, 'g')
+        lalini, = graph.plot(xalt, yalt, 'g--')
+        graph.plot(xdata, ydata, 'go', markersize = 7, alpha = 0.15)
+        
+        plt.axis([0, maxis, ylowest, yhighest])
+        graph.set_title('Weight Loss Over ' + month, fontname = 'Ubuntu Mono', fontsize = 32, fontweight = 'bold')
+        graph.set_xlabel('Time', fontname = 'Ubuntu Mono', fontsize = 16)
+        graph.set_ylabel('Weight (lb.)', fontname = 'Ubuntu Mono', fontsize = 16)
+        graleg = graph.legend((lideal, lamove, lalini), ('Ideal weight, linear average', 'Actual weight, moving average', 'Actual weight, linear average'))
+        plt.setp(graleg.texts, fontname = 'Ubuntu Mono', fontsize = 16)
+        plt.show()
+        print()
         sys.exit()
 
-    xhi = np.amax(xdata)
-    xlo = np.amin(xdata)
-    yhi = np.amax(ydata) + 2.5
-    ylo = np.amin(ydata) - 2.5
-
-    month = cal.month_name[mint]
-    maxis = cal.monthrange(2017, mint)[1]
-
-    xply = np.polyfit(xdata, ydata, xhi)
-    yply = np.poly1d(xply)
-    xlin = np.polyfit(xdata, ydata, 1)
-    ylin = np.poly1d(xlin)
-
-    xnew = np.linspace(xlo, xhi, 300)
-    xalt = np.linspace(xlo, xhi, 50)
-    ynew = yply(xnew)
-    yalt = ylin(xalt)
-
-    blinx = np.linspace(xlo, maxis, 50)
-    bliny = eval('-5*blinx/19 + 203')
-    
-    if np.amax(bliny) + 2.5 > yhi:
-        yhighest = np.amax(bliny) + 2.5
+    elif selectop is 1:
+        twrite(col.y + '\nPlease enter a month to inject data: ' + col.x)
         
+        while True:
+            try:
+                mint = int(input())
+                def find_file(name):
+                    for files in os.getcwd():
+                        glog = str(name) + 'vals.log'
+                        return str(name)
+
+            except ValueError:
+                twrite(col.r + '\nInput was not an integer. Please retry: ' + col.x)
+                continue
+
+            if not (1 <= mint <= 12):
+                twrite(col.r + '\nInput was not a valid integer. Please retry: ' + col.x)
+                continue
+            
+            try:
+                mstr = find_file(mint)
+                fto = mstr + 'vals.log'
+                os.path.isfile(fto)
+                fr = open(fto, 'r')
+
+            except IOError:
+                while True:
+                    month = cal.month_name[mint]
+                    twrite(col.y + '\nThe specified Geweva log was not found.\n' + col.x + \
+                    col.c + 'Would you like to create a new log for the month of ' + month + '? [Y/n] ' + col.x)
+                    prompt = input()
+
+                    if prompt.lower() in ('yes', 'ye', 'y', ''):
+                        twrite(col.c + '\nCreating new file')
+                        time.sleep(0.5)
+                        twrite('.')
+                        time.sleep(0.7)
+                        twrite('.')
+                        time.sleep(0.7)
+                        twrite('.\n' + col.x)
+
+                        fwp = open(fto, 'w+')
+                        fwp.close()
+                        time.sleep(0.5)
+
+                        twrite(col.g + '\nSuccess! ' + fto + ' was created.\n' + col.x)
+                        break
+
+                    elif prompt.lower() in ('no', 'n'):
+                        twrite(col.r + '\nExiting...\n\n' + col.x)
+                        sys.exit()
+                    
+                    else:
+                        twrite(col.r + '\nPlease respond with "yes/y" or "no/n".\n' + col.x)
+                        continue
+                break
+
+            else:
+                break
+
+        mstr = find_file(mint)
+        fto = mstr + 'vals.log'
+        os.path.isfile(fto)
+        fr = open(fto, 'r')
+        alldata = fr.read().splitlines()
+        fr.close()
+
+        twrite(col.y + '\nEnter a numerical time value: ' + col.x)
+
+        while True:
+            xapraw = input()
+
+            try:
+                xapflo = float(xapraw)
+
+            except ValueError:
+                twrite(col.r + '\nThe specified value was not valid. Please retry: ' + col.x)
+                continue
+
+            else:
+                break
+
+        twrite(col.y + '\nEnter a numerical weight value: ' + col.x)
+
+        while True:
+            yapraw = input()
+
+            try:
+                yapflo = float(yapraw)
+
+            except ValueError:
+                twrite(col.r + '\nThe specified value was not valid. Please retry: ' + col.x)
+                continue
+
+            else:
+                break
+
+        if os.stat(fto).st_size > 0:
+            xdapd = ''.join(alldata[0]) + '   ' + str(xapflo) + '\n'
+            ydapd = ''.join(alldata[1]) + '   ' + str(yapflo)
+            
+            twrite(col.g + '\nInput (' + str(xapflo) + ', ' + str(yapflo) + ') accepted.\n\n' + col.x)
+
+            fw = open(fto, 'w')
+            fw.truncate()
+            fw.write(xdapd)
+            fw.write(ydapd)
+            fw.close()
+
+        elif os.stat(fto).st_size == 0:
+            xdapd = str(xapflo) + '\n'
+            ydapd = str(yapflo)
+            
+            twrite(col.g + '\nInput (' + str(xapflo) + ', ' + str(yapflo) + ') accepted.\n\n' + col.x)
+
+            fw = open(fto, 'w')
+            fw.truncate()
+            fw.write(xdapd)
+            fw.write(ydapd)
+            fw.close()
+        sys.exit()
+
+    elif selectop is 9:
+        twrite(col.r + '\nExiting...\n\n' + col.x)
+        sys.exit()
+    
     else:
-        yhighest = yhi
-    
-    if np.amin(bliny) - 2.5 < ylo:
-        ylowest = np.amin(bliny) - 2.5
-
-    else:
-        ylowest = ylo
-    
-    twrite(col.c + '\nGraphing data')
-    time.sleep(0.5)
-    twrite('.')
-    time.sleep(0.7)
-    twrite('.')
-    time.sleep(0.7)
-    twrite('.\n\n' + col.x)
-    time.sleep(0.5)
-
-    # dates = mcal.date2num('2017-' + mstr + '-1', '2017-' + mstr + '-' + str(maxis))
-
-    fig, graph = plt.subplots()
-    graph.fill_between(blinx, bliny - 2.5, bliny + 2.5, color = 'b', alpha = 0.05)
-    lideal, = graph.plot(blinx, bliny, 'b')
-    lamove, = graph.plot(xnew, ynew, 'g')
-    lalini, = graph.plot(xalt, yalt, 'g--')
-    graph.plot(xdata, ydata, 'go', markersize = 7, alpha = 0.15)
-    
-    plt.axis([0, maxis, ylowest, yhighest])
-    graph.set_title('Weight Loss Over ' + month, fontname = 'Ubuntu Mono', fontsize = 32, fontweight = 'bold')
-    graph.set_xlabel('Time', fontname = 'Ubuntu Mono', fontsize = 16)
-    graph.set_ylabel('Weight (lb.)', fontname = 'Ubuntu Mono', fontsize = 16)
-    graleg = graph.legend((lideal, lamove, lalini), ('Ideal weight, linear average', 'Actual weight, moving average', 'Actual weight, linear average'))
-    plt.setp(graleg.texts, fontname = 'Ubuntu Mono', fontsize = 16)
-    plt.show()
-
-elif selectop is 1:
-    twrite(col.y + '\nPlease enter a month to inject data: ' + col.x)
-    
-    while True:
-        try:
-            mint = int(input())
-            def find_file(name):
-                for files in os.getcwd():
-                    glog = str(name) + 'vals.log'
-                    return str(name)
-
-        except ValueError:
-            twrite(col.r + '\nInput was not an integer. Please retry: ' + col.x)
-            continue
-
-        if not (1 <= mint <= 12):
-            twrite(col.r + '\nInput was not a valid integer. Please retry: ' + col.x)
-            continue
-        
-        try:
-            mstr = find_file(mint)
-            fto = mstr + 'vals.log'
-            os.path.isfile(fto)
-            fr = open(fto, 'r')
-
-        except IOError:
-            while True:
-                month = cal.month_name[mint]
-                twrite(col.y + '\nThe specified Geweva log was not found.\n' + col.x + \
-                col.c + 'Would you like to create a new log for the month of ' + month + '? [Y/n] ' + col.x)
-                prompt = input()
-
-                if prompt.lower() in ('yes', 'ye', 'y', ''):
-                    twrite(col.c + '\nCreating new file')
-                    time.sleep(0.5)
-                    twrite('.')
-                    time.sleep(0.7)
-                    twrite('.')
-                    time.sleep(0.7)
-                    twrite('.\n' + col.x)
-
-                    fwp = open(fto, 'w+')
-                    fwp.close()
-                    time.sleep(0.5)
-
-                    twrite(col.g + '\nSuccess! ' + fto + ' was created.\n' + col.x)
-                    break
-
-                elif prompt.lower() in ('no', 'n'):
-                    twrite(col.r + '\nExiting...\n\n' + col.x)
-                    sys.exit()
-                
-                else:
-                    twrite(col.r + '\nPlease respond with "yes/y" or "no/n".\n' + col.x)
-                    continue
-            break
-
-        else:
-            break
-
-    mstr = find_file(mint)
-    fto = mstr + 'vals.log'
-    os.path.isfile(fto)
-    fr = open(fto, 'r')
-    alldata = fr.read().splitlines()
-    fr.close()
-
-    twrite(col.y + '\nEnter a numerical time value: ' + col.x)
-
-    while True:
-        xapraw = input()
-
-        try:
-            xapflo = float(xapraw)
-
-        except ValueError:
-            twrite(col.r + '\nThe specified value was not valid. Please retry: ' + col.x)
-            continue
-
-        else:
-            break
-
-    twrite(col.y + '\nEnter a numerical weight value: ' + col.x)
-
-    while True:
-        yapraw = input()
-
-        try:
-            yapflo = float(yapraw)
-
-        except ValueError:
-            twrite(col.r + '\nThe specified value was not valid. Please retry: ' + col.x)
-            continue
-
-        else:
-            break
-
-    if os.stat(fto).st_size > 0:
-        xdapd = ''.join(alldata[0]) + '   ' + str(xapflo) + '\n'
-        ydapd = ''.join(alldata[1]) + '   ' + str(yapflo)
-        
-        twrite(col.g + '\nInput (' + str(xapflo) + ', ' + str(yapflo) + ') accepted.\n\n' + col.x)
-
-        fw = open(fto, 'w')
-        fw.truncate()
-        fw.write(xdapd)
-        fw.write(ydapd)
-        fw.close()
-
-    elif os.stat(fto).st_size == 0:
-        xdapd = str(xapflo) + '\n'
-        ydapd = str(yapflo)
-        
-        twrite(col.g + '\nInput (' + str(xapflo) + ', ' + str(yapflo) + ') accepted.\n\n' + col.x)
-
-        fw = open(fto, 'w')
-        fw.truncate()
-        fw.write(xdapd)
-        fw.write(ydapd)
-        fw.close()
-
-elif selectop is 9:
-    twrite(col.r + '\nExiting...\n\n' + col.x)
+        twrite(col.r + '\nIncorrect operation specified. Please retry.\n' + col.x)
+        continue
